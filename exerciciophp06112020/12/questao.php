85ce92a7750c92c62be719b86feeb9e05b560f63
everton <evertonjunior57@gmail.com>
2020-11-23 20:38:18 -0300

    exercicio

---
+++ exerciciophp06112020/12/questao.php
@@ -0,0 +1,30 @@
+<?php
+
+$numero = $_GET['numero'];
+
+
+
+if ($numero  >100 ){
+
+echo $numero;
+
+}
+elseif($numero <200){
+
+    echo $numero;
+}
+else{
+
+    echo "encerrado";
+}
+
+
+
+
+
+
+
+
+
+
+?>
